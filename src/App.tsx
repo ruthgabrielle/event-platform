e7359507f706c23f78de79bfc384d83f66f3114c
ruthgabrielle <ruthfgms@gmail.com>
2022-06-24 16:05:40 -0300

    Setup platform

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
+function App() {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+  console.log(data);
+  return (
+    <div>
+      <ul>
+        {data?.lessons.map((lesson) => (
+          <li key={lesson.id}> {lesson.title} </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
