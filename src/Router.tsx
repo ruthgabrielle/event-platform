a2723d38e1f5a66a816fe26a2ea9cd1708050601
ruthgabrielle <ruthfgms@gmail.com>
2022-06-27 14:50:45 -0300

    Router and player

---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import { Event } from "./pages/Event";
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home Page </h1>} />
+      <Route path="/event" element={<Event />} />
+      <Route path="/event/lesson/:slug" element={<Event />} />
+    </Routes>
+  );
+}
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,17 @@
+import { Video } from "@vime/react";
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
-import { Player } from "../components/Player";
+import { VideoPlayer } from "../components/Player";
 import { Sidebar } from "../components/Sidebar";
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <Player />
+        {slug ? <VideoPlayer lessonSlug={slug} /> : <div className="flex-1" />}{" "}
         <Sidebar />
       </main>
     </div>
